0e75af77a300028a1b0023b4e4bc02af2b39711b
Luca Fabri <luca.fabri1999@gmail.com>
2023-11-16 13:28:26 +0100

    test: pytest

--- src/test/controllers/environment/observation_test.py
+++ src/test/controllers/environment/observation_test.py
@@ -1,20 +1,13 @@
 import unittest
 
-from main.controllers.environment.environment_observer import EnvironmentObserver
-from main.model.agents.predator import Predator
-from main.model.agents.prey import Prey
-from main.model.environment import Environment
+from src.main.controllers.environment.environment_observer import EnvironmentObserver
+from src.main.model.agents.predator import Predator
+from src.main.model.agents.prey import Prey
 
 
 class ObservationTest(unittest.TestCase):
 
     env_observer = EnvironmentObserver()
-
-    def test_simple_observation(self):
-        coordinates = [(10, 10), (10, 8), (10, 12), (8, 10), (10, 8)]
-        env: Environment = Environment(agents=[Predator("%d".format(i), x, y) for i, (x, y) in enumerate(coordinates)])
-        self.assertEqual(self.env_observer.observe(env.agents[0], env),
-                         ([1.7, -1, -1, 1.7, -1, -1, 1.7, -1, -1, -1, 1.7, -1, -1, -1], 1))
 
     def test_simple_eating(self):
         predator = Predator(id="predator-1", x=2, y=2)
