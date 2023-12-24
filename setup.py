95a9180c299836c0f2394a05d51d8d3a8fe3da47
Luca Fabri <luca.fabri1999@gmail.com>
2023-12-24 10:12:40 +0100

    chore: edit setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="marl-predator-prey",
-    version="0.1",
+    name="predator-prey-app",
+    version="0.1.0",
     description="Distributed RL training application",
     url="#",
     author="Luca Fabri",
-    author_email="luca.fabri@studio.unibo.it",
+    author_email="luca.fabri1999@gmail.com",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     setup_requires=["pytest-runner"],
