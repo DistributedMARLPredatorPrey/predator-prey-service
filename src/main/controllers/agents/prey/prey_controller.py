fae7184f773ccbbaac53b792fdc1f37a378b0567
Luca Fabri <luca.fabri1999@gmail.com>
2023-12-24 17:05:57 +0100

    chore: math format doesn't work

--- src/main/controllers/agents/prey/prey_controller.py
+++ src/main/controllers/agents/prey/prey_controller.py
@@ -17,9 +17,9 @@ class PreyController(AgentController):
         The prey reward is an exponential function that gets higher as the distance
         of the closest predator increases.
         Furthermore, it's a min-max normalized function, so that when the distance is 0 the reward is 0,
-        and when it's the maximum visual depth it's equal to 1:
+        and when it's the maximum visual depth it's equal to 1: f(x, d) = \frac{1 - e^{-x}}{1- e^{-d}}
 
-        .. math:: f(x, d) = \frac{1 - e^{-x}}{1- e^{-d}}
+        .. math:: p(x) = \frac{e^{\kappa cos(x-\mu)}}{2\pi I_0(\kappa)}
 
         """
         return (1 - np.power(np.e, -np.min(self.last_state.distances))) / (
