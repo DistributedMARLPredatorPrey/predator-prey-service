5e3f02ed9037f84b746e3ea5054f2c7c70c2f62d
Luca Fabri <luca.fabri1999@gmail.com>
2024-07-09 14:12:19 +0200

    refactor: data sending to buffer service

--- src/main/controllers/replay_buffer/replay_buffer_controller.py
+++ src/main/controllers/replay_buffer/replay_buffer_controller.py
@@ -1,9 +1,6 @@
 import json
 
-import numpy as np
 import requests
-
-from src.main.model.agents.agent_type import AgentType
 
 
 class ReplayBufferController:
@@ -11,17 +8,7 @@ class ReplayBufferController:
         self._host = host
         self._port = port
 
-    def record(self, record_tuples):
-        self.__send_prey_record_tuple(record_tuples[AgentType.PREY])
-        self.__send_predator_record_tuple(record_tuples[AgentType.PREDATOR])
-
-    def __send_prey_record_tuple(self, record_tuple):
-        self.__send_record_tuple("record_data/prey/", record_tuple)
-
-    def __send_predator_record_tuple(self, record_tuple):
-        self.__send_record_tuple("record_data/predator/", record_tuple)
-
-    def __send_record_tuple(self, route, record_tuple):
+    def record(self, record_tuple):
         prev_states, actions, rewards, next_states = record_tuple
         record_json = {
             "State": [[float(ps) for prev_state in prev_states for ps in prev_state]],
@@ -32,4 +19,6 @@ class ReplayBufferController:
             ],
         }
         json_data = json.dumps(record_json)
-        requests.post(f"http://{self._host}:{self._port}/{route}", json=json_data)
+        requests.post(
+            f"http://{self._host}:{self._port}/record_data/predator/", json=json_data
+        )
