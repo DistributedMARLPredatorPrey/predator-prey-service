06263974155e7a9fed64f973bc3f018ea4952a2a
Luca Fabri <luca.fabri1999@gmail.com>
2024-07-15 23:09:20 +0200

    fix: setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="predator-prey-service",
-    version="0.1.0",
+    version="0.2.0",
     description="Predator Prey Service",
     url="#",
     author="Luca Fabri",
@@ -17,5 +17,5 @@ setup(
     test_suite="src.test",
     install_requires=requirements,
     zip_safe=False,
-    python_requires="==3.11",
+    #python_requires="==3.11",
 )
